d1c5684fa84eff484f591817baea9bf18b407fe9
Guest User <Guest@Epicodus-8A.local>
2018-07-06 12:02:33 -0700

    Change BeerList to class component

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -2,41 +2,56 @@ import React from 'react';
 import OnTap from './OnTap';
 
 
-function BeerList(){
-  const beerList = {
-    padding: '1px',
-  };
+class BeerList extends React.Component {
 
-  const green = {
-    backgroundColor: '#28a745',
-    width: '300px',
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterBeerList: []
+    };
+    this.handleSellingBeer = this.handleSellingBeer.bind(this);
+  }
 
-  const yellow = {
-    backgroundColor: '#ffc108',
-    width: '300px',
-  };
+  handleSellingBeer(soldBeer){
 
-  const blue = {
-    backgroundColor: '#17a2b8',
-    width: '300px',
-  };
+  }
 
+render(){
   return (
-    <div style={beerList}>
-      <div style={green}>
+    <div>
+      <style jsx>{`
+        div {
+          padding: 1px;
+        }
+
+        #green {
+          background-color: #28a745;
+          width: 300px;
+        }
+
+        #yellow {
+          background-color: #ffc108;
+          width: 300px;
+        }
+
+        #blue {
+          background-color: #17a2b8;
+          width: 300px;
+        }
+            `}</style>
+          <div id="green">
         <OnTap
           title="Guinness --- Stout"
           msg="$5.00 Pints: 124"
           alc="ABV: 3.8%"/>
       </div>
-      <div style={yellow}>
+      <div id='yellow'>
         <OnTap
           title="Newcastle --- Ale"
           msg="$4.00 Pints: 124"
           alc="ABV: 4.0%"/>
       </div>
-      <div style={blue}>
+      <div id='blue'>
         <OnTap
           title="Yuengling --- Lager"
           msg="$2.50 Pints: 124"
@@ -44,6 +59,8 @@ function BeerList(){
       </div>
     </div>
   );
+}
+
 }
 
 export default BeerList;
